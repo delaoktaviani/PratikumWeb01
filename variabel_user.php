c6f19d5f2acb97bd44fbd7f7c473bb365dbf3db8
delaoktaviani <92714055+delaoktaviani@users.noreply.github.com>
2022-03-01 22:31:52 +0700

    Add files via upload

---
+++ variabel_user.php
@@ -0,0 +1,13 @@
+<?php
+    // definisikan variables
+    $nama = 'Dela Oktaviani';
+    $umur = 20;
+    $berat = 46.8;
+
+    echo 'Nama : ' . $nama;
+    echo '<br/>Umur : ' . $umur.' Tahun';
+    echo '<br/>Berat : '.$berat.' Kg';
+
+    echo "<br/Hello $nama Apakabar";
+
+    ?>
